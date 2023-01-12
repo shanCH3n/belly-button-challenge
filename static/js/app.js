bbf0f4b6cf3f6e485dce6887dacc8cfdb90e1547
shanCH3n <111955977+shanCH3n@users.noreply.github.com>
2023-01-12 11:30:05 +0800

    attempted gauge charts

--- static/js/app.js
+++ static/js/app.js
@@ -7,7 +7,7 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 const dataPromise = d3.json(url);
 
 
-// Review sample.json to check keys of interes (e.g. otu_labels)
+// Review sample.json to check keys of interest (e.g. otu_labels)
 // Create charts function.
 
 function charts(selectedID) {
@@ -25,7 +25,7 @@ function charts(selectedID) {
         var labels = result.otu_labels;
         var values = result.sample_values;
 
-        // Build a horizontal bar chart//
+        // Build a horizontal bar chart //
         var trace1 = {
             x: values.slice(0, 10).reverse(), // Pick out top 10 most prevalent OTUs to display
             y: ids.slice(0, 10).map((otuID) => `OTU ${otuID}`).reverse(),
@@ -37,15 +37,15 @@ function charts(selectedID) {
         var bar_data = [trace1];
 
         var bar_layout = {
-            title: "Top 10 Most Common MicroBacteria found in Subject's Navel",
-            margin: {t: 60, l: 160},
+            title: "<b>Top 10 MicroBacteria in Subject's Navel</b>",
+            margin: {t: 50, l: 160},
         };
 
 
         Plotly.newPlot("bar", bar_data, bar_layout);
 
         
-        // Create a bubble chart//
+        // Create a bubble chart //
         var trace2 = {
             x: ids,
             y: values,
@@ -68,14 +68,14 @@ function charts(selectedID) {
         };
 
         Plotly.newPlot("bubble", bubble_data, bubble_layout);
+
     });
 };
 
-// TBC Bonus gauge chart
 
 // Display sample metadata, i.e., an individual's demographic information.
 
-function MetaDemo(sample) {
+function DemoInfo(sample) {
     dataPromise.then((data) => {
         var Metadata = data.metadata;
         var resultsarray = Metadata.filter(sampleobject =>
@@ -92,6 +92,50 @@ function MetaDemo(sample) {
 };
 
 
+// Create gauge chart ADVANCED //
+
+function gaugeChart(sample) {
+    dataPromise.then((data) => {
+        var Metadata = data.metadata; // washing frequency 'wfreq' data is located within 'metadata'.
+        var subjectArray = Metadata.filter(sampleObj =>
+            sampleObj.id == sample);
+        var result = subjectArray[0];
+
+        var gauge_data = [
+            {
+            type: "indicator",
+            mode: "gauge+number",
+            value: result.wfreq,
+            title: { text: "<b>Belly Button Washing Frequency</b> <br>Scrubs per Week</br>", font: { size: 16 } },
+            gauge: {
+                axis: { range: [null, 9], tickwidth: 1, tickcolor: "black"},
+                bgcolor: "white",
+                steps: [
+                    { range: [0, 1], color: "rgb(248, 243, 236)" },
+                    { range: [1, 2], color: "rgb(239, 234, 220)" },
+                    { range: [2, 3], color: "rgb(230, 225, 205)" },
+                    { range: [3, 4], color: "rgb(218, 217, 190)" },
+                    { range: [4, 5], color: "rgb(204, 209, 176)" },
+                    { range: [5, 6], color: "rgb(189, 202, 164)" },
+                    { range: [6, 7], color: "rgb(172, 195, 153)" },
+                    { range: [7, 8], color: "rgb(153, 188, 144)" },
+                    { range: [8, 9], color: "rgb(132, 181, 137)" },
+                ],
+            },
+        }];
+        
+    
+        var gauge_layout = {
+            width: 450,
+            height: 450,
+            margin: { t: 50, r: 25, l: 25, b: 25 },
+            paper_bgcolor: "white"
+        };
+
+        Plotly.newPlot("gauge", gauge_data, gauge_layout);
+    });
+};
+
 // Initialize default plot.
 
 function init() {
@@ -107,7 +151,8 @@ function init() {
 
         const firstSample = data.names[0];
         charts(firstSample);
-        MetaDemo(firstSample);
+        DemoInfo(firstSample);
+        gaugeChart(firstSample);
     });
 };
 
@@ -119,7 +164,8 @@ console.log("Data Promise: ", dataPromise);
 
 function optionChanged(newSample) {
     charts(newSample);
-    MetaDemo(newSample);
+    DemoInfo(newSample);
+    gaugeChart(newSample);
 }
 
 init();
